6b0b5f92921826fd8aa9ad5d5cf7c3d640be5828
lhoju0158 <lhoju0158@gmail.com>
2025-08-03 14:46:54 +0900

    first commit

---
+++ src/test/java/com/exam/jwtex01/Jwtex01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.jwtex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwtex01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
